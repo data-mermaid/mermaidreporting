76e45b29a5813be8be52d6353f5f6bf50b2a091b
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-05-12 14:04:50 -0400

    r cmd check fix

--- tests/testthat/test-mermaid_boxplot.R
+++ tests/testthat/test-mermaid_boxplot.R
@@ -25,27 +25,27 @@ test_that("mermaid_plot_fish_belt_biomass values cleaning works as expected", {
   expect_true(all(unique(p$data[["group"]]) == c("Piscivore", "Invertivore Mobile")))
 
   p <- events %>%
-    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, .clean_values = FALSE)
+    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, clean_values = FALSE)
   expect_true(all(unique(p$data[["reef_exposure"]]) == c("exposed", "semi-exposed")))
   expect_true(all(unique(p$data[["group"]]) == c("piscivore", "invertivore-mobile")))
 
   p <- events %>%
-    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, .clean_values_case = "sentence")
+    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, clean_values_case = "sentence")
   expect_true(all(unique(p$data[["reef_exposure"]]) == c("Exposed", "Semi exposed")))
   expect_true(all(unique(p$data[["group"]]) == c("Piscivore", "Invertivore mobile")))
 
   p <- events %>%
-    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, .clean_values_case = "sentence", .replace_dashes = FALSE)
+    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, clean_values_case = "sentence", replace_dashes = FALSE)
   expect_true(all(unique(p$data[["reef_exposure"]]) == c("Exposed", "Semi-exposed")))
   expect_true(all(unique(p$data[["group"]]) == c("Piscivore", "Invertivore-mobile")))
 
   p <- events %>%
-    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, .replace_dashes = FALSE)
+    mermaid_plot_fish_belt_biomass(biomass_kgha_by_trophic_group_avg, reef_exposure, replace_dashes = FALSE)
   expect_true(all(unique(p$data[["reef_exposure"]]) == c("Exposed", "Semi-Exposed")))
   expect_true(all(unique(p$data[["group"]]) == c("Piscivore", "Invertivore-Mobile")))
 })
 
-test_that("mermaid_plot_fish_belt_biomass errors if .group_var is not a df-col", {
+test_that("mermaid_plot_fish_belt_biomass errors if group_var is not a df-col", {
   events_clean <- tibble::tibble(
     biomass_kgha_by_trophic_group_avg = c(10, 100),
     trophic_group = c("piscivore", "invertivore-mobile"),
