857593246eeb3355363291ca404f0cd4a79a012b
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-05-12 12:40:14 -0400

    Add fiji mapping and manual lat/long tests

--- tests/testthat/test-mermaid_map_sites_static.R
+++ tests/testthat/test-mermaid_map_sites_static.R
@@ -1,12 +1,12 @@
 library(proto)
 library(ggplot2)
 library(mermaidr)
+library(dplyr)
 
 sample_events <- mermaid_search_projects(name = "XPDC Kei Kecil 2018") %>%
   mermaid_get_project_data("fishbelt", "sampleevents", limit = 25)
 
 test_that("mermaid_map_sites_static returns a map containing all the points", {
-
   p <- mermaid_map_sites_static(sample_events)
   expect_equal(nrow(ggplot_build(p)$data[[2]]), 25) # TODO: better way to extract this, so next test passes
 
@@ -51,4 +51,22 @@ test_that("mermaid_static_map_bounds returns bounds for a static map", {
   expect_named(b, c("latitude_bounds", "longitude_bounds"))
   expect_equal(round(b[[1]]), c(-6, -5))
   expect_equal(round(b[[2]]), c(132, 133))
+})
+
+test_that("mermaid_static_map_bounds has correct CRS for input data", {
+  p <- mermaid_map_sites_static(sample_events)
+  gb <- ggplot_build(p)
+  expect_equal(sf::st_crs(p$layers[[2]]$data)$epsg, 4326)
+
+  fiji_sites <- mermaid_get_sites() %>%
+    filter(country == "Fiji") %>%
+    head(10)
+  p <- mermaid_map_sites_static(fiji_sites, use_fiji_crs = TRUE)
+  gb <- ggplot_build(p)
+  expect_equal(sf::st_crs(p$layers[[2]]$data)$epsg, 3460)
+})
+
+test_that("set lat/long bounds are used, and warn if they exclude points", {
+  expect_warning(mermaid_map_sites_static(sample_events, latitude_bounds = c(-6, -5.8), longitude_bounds = c(132, 133)), "Not all sites are within")
+  expect_equal(mermaid_static_map_bounds(mermaid_map_sites_static(sample_events, latitude_bounds = c(-6, -5.8), longitude_bounds = c(132, 133))), list(latitude_bounds = c(-6, -5.8), longitude_bounds = c(132, 133)))
 })
